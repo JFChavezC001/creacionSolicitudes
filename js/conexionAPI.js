93df681425fb3880d47537c1d2e3530b9daee772
JFChavezC001 <jfchavezc001@gmail.com>
2024-05-27 21:41:36 -0600

    creamos una nueva funcion la cual sirve para agregar nuevos videos a traves del metodo POST, especificamos el metodo, encabezado y el cuerpo que incluira un nuevo video

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,13 +1,25 @@
 async function listarVideos(){
     const conexion = await fetch("http://localhost:3001/videos");
-    
-
     const conexionConvertida=conexion.json();
     //console.log(conexionConvertida);
-    
     return conexionConvertida
-    
 }
+
+async function crearVideo(titulo, descripcion, imagen, url){
+    const conexion = await fetch("http://localhost:3001/videos",{
+        method: "POST", //Especificamos metodo
+        headers:{"content-type":"aplication/json"},//conjunto de info relacionada al archivo mediante consulta
+        body:JSON.stringify({
+            titulo: titulo,
+            descripcion: descripcion,
+            url: url,
+            imagen: imagen
+        })//contiene el cuerpo de como estara descrito nuestro 
+    });//colocamos la url donde encontramos el archivo json, ademas de que colocamos la peticion tipo POST
+
+    const conexionConvertida= conexion.json();
+    return conexionConvertida;
+
 export const conexionAPI={
     listarVideos
 }
